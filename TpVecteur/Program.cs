1411ff57cb9c52327da4c47f11e34fa0fbd93ded
SylvainTorrenti <torrsyl@gmail.com>
2023-01-18 10:52:15 +0100

    Presentation

--- TpVecteur/Program.cs
+++ TpVecteur/Program.cs
@@ -3,7 +3,8 @@
 #region Vector 2D
 Vector2D Vector2D1 = new Vector2D(1, 2);
 Vector2D Vector2D2 = new Vector2D(1, 2);
-Console.WriteLine("****Vecteur 2D****");
+Console.WriteLine("             ****Vecteur 2D****");
+Console.WriteLine();
 Console.WriteLine($"Le vector 1 est : {Vector2D1.ToString()}");
 Console.WriteLine($"La norme est : {Vector2D1.Norme()}");
 Console.WriteLine("-----------");
@@ -12,8 +13,10 @@ Console.WriteLine($"La norme est : {Vector2D2.Norme()}");
 Console.WriteLine("-----------");
 Vector2D1.Equals2D(Vector2D2, Vector2D1);
 Console.WriteLine("-----------");
+Console.WriteLine();
 #endregion
-Console.WriteLine("****Vecteur 3D****");
+Console.WriteLine("             ****Vecteur 3D****");
+Console.WriteLine();
 Vector3D Vector3D1 = new Vector3D(1, 1, 1);
 Vector3D Vector3D2 = new Vector3D(1, 2, 1);
 Console.WriteLine($"Le vector 1 est : {Vector3D1.ToString()}");
